ca672db12e4434c1f5e274bdb13bdbbb861ceec1
Kristina Hengster Tintor and Michelle Morin <michelle@>
2020-02-26 16:04:48 -0800

    refactor logic and add colors to console

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace Wildlife
   {
     public static void ReturnToMenu()
     {
-      Console.WriteLine("Would you like to return to the main menu? (Y/N)");
+      Console.WriteLine("Would you like to return to the main menu?");
+      Console.WriteLine("[Y] or [N]");
       string searchAgain = Console.ReadLine();
       string searchAgainLower = searchAgain.ToLower();
       if (searchAgainLower == "y")
@@ -23,6 +24,9 @@ namespace Wildlife
 
     public static void AddAnimal()
     {
+      Console.ResetColor();
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.ForegroundColor = ConsoleColor.Magenta;
       Console.WriteLine("What species of animal would you like to add to the tracker?");
       string animalSpecies = Console.ReadLine();
       string animalSpeciesLower = animalSpecies.ToLower();
@@ -34,19 +38,15 @@ namespace Wildlife
       int animalAge = int.Parse(stringAnimalAge);
       Animal newAnimal = new Animal(animalSpeciesLower, animalNameLower, animalAge);
       Animal.myAnimals.Add(newAnimal);
-      Console.WriteLine("We're now tracking " + animalName + "! Here are all the animals being tracked:");
-      foreach(Animal animal in Animal.myAnimals)
-      {
-        Console.WriteLine("----------------------");
-        Console.WriteLine("Name " + animal.GetAnimalName());
-        Console.WriteLine("Species " + animal.GetAnimalType());
-        Console.WriteLine("Age: " + animal.GetAnimalAge());
-      }
+      displayTrackedAnimals();
       ReturnToMenu();
     }
 
     public static void UpdateAnimalAge()
     {
+      Console.ResetColor();
+      Console.BackgroundColor = ConsoleColor.Yellow;
+      Console.ForegroundColor = ConsoleColor.Blue;
       Console.WriteLine("Would you like to update the age for any of these animals? Enter the animal's name or species:");
       string updateResponse = Console.ReadLine();
       string updateResponseLower = updateResponse.ToLower();
@@ -82,6 +82,8 @@ namespace Wildlife
           Console.WriteLine("Name: " + animalsMatchingUpdate[index].GetAnimalName());
           Console.WriteLine("Species: " + animalsMatchingUpdate[index].GetAnimalType());
           Console.WriteLine("Age: " + animalsMatchingUpdate[index].GetAnimalAge());
+          Console.WriteLine("----------------------");
+
         }
         string animalToUpdate = Console.ReadLine();
         int animalNumber = int.Parse(animalToUpdate);
@@ -96,63 +98,114 @@ namespace Wildlife
 
     public static void FindAnimal()
     {
-      Console.WriteLine("Which animal are you looking for?");
-      string animalResponse = Console.ReadLine();
-      string animalResponseLower = animalResponse.ToLower();
-      List<Animal> animalsMatchingSearch = new List<Animal>() {};
-      foreach (Animal animal in Animal.myAnimals)
+      Console.ResetColor();
+      Console.BackgroundColor = ConsoleColor.DarkGray;
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("Would you like to search for a specific animal?");
+      Console.WriteLine("Y or N");
+      string yesOrNo = Console.ReadLine();
+      if (yesOrNo.ToLower() == "y")
       {
-        if (animal.GetAnimalName() == animalResponseLower || animal.GetAnimalType() == animalResponseLower)
+        Console.WriteLine("Which animal are you looking for?");
+        string animalResponse = Console.ReadLine();
+        string animalResponseLower = animalResponse.ToLower();
+        List<Animal> animalsMatchingSearch = new List<Animal>() {};
+        foreach (Animal animal in Animal.myAnimals)
         {
-          animalsMatchingSearch.Add(animal);
+          if (animal.GetAnimalName() == animalResponseLower || animal.GetAnimalType() == animalResponseLower)
+          {
+            animalsMatchingSearch.Add(animal);
+          }
         }
-      }
-      if (animalsMatchingSearch.Count == 0)
-      {
-        Console.WriteLine("There are no animals matching your search!");
-        ReturnToMenu();
+      displaySearchResults(animalsMatchingSearch);
       }
       else
       {
-        Console.WriteLine("The following animals matched your search:");
-        foreach(Animal animal in animalsMatchingSearch)
+        displayTrackedAnimals();
+      }
+      ReturnToMenu();
+    }
+
+    public static void displayTrackedAnimals()
+    {
+      if (Animal.myAnimals.Count > 0)
+      {
+        Console.WriteLine("Animals currently being tracked include:");
+        foreach(Animal animal in Animal.myAnimals)
         {
           Console.WriteLine("----------------------");
           Console.WriteLine("Name: " + animal.GetAnimalName());
           Console.WriteLine("Species: " + animal.GetAnimalType());
           Console.WriteLine("Age: " + animal.GetAnimalAge());
+          Console.WriteLine("----------------------");
         }
+      }
+      else
+      {
+        Console.WriteLine("There are no animals currently being tracked.");
+      }
+    }
+
+    public static void displaySearchResults(List<Animal> searchResults)
+    {
+      if (searchResults.Count == 0)
+      {
+        Console.WriteLine("There are no animals matching your search!");
+        displayTrackedAnimals();
+      }
+      else
+      {
+        Console.WriteLine("The following animals matched your search:");
+        foreach(Animal animal in searchResults)
+        {
+          Console.WriteLine("----------------------");
+          Console.WriteLine("Name: " + animal.GetAnimalName());
+          Console.WriteLine("Species: " + animal.GetAnimalType());
+          Console.WriteLine("Age: " + animal.GetAnimalAge());
+          Console.WriteLine("----------------------");
+        }
+      }
+    }
+
+    public static void SelectOption()
+    {
+      Console.WriteLine("Welcome to the main menu of wildlife park tracker! Would you like to:");
+      Console.WriteLine("- track an animal in the park");
+      Console.WriteLine("- add an animal to the wildlife tracker,");
+      Console.WriteLine("- update information for a tracked animal, or");
+      Console.WriteLine("- quit the application?");
+      Console.WriteLine("ENTER: TRACK, ADD, UPDATE, or QUIT");
+      string userAnswer = Console.ReadLine();
+      string userAnswerLower = userAnswer.ToLower();
+      if (userAnswerLower == "track")
+      {
+        FindAnimal();
+        displayTrackedAnimals();
+      }
+      else if (userAnswerLower == "add")
+      {
+        AddAnimal();
+      }
+      else if (userAnswerLower == "update")
+      {
+        UpdateAnimalAge();
+      }
+      else if (userAnswerLower == "quit")
+      {
+        Console.WriteLine("Have a nice day!");
+      }
+      else
+      {
         ReturnToMenu();
       }
     }
 
     public static void Main()
     {
-      Console.WriteLine("Welcome to the main menu of wildlife park tracker! Would you like to track an animal in the park? (Y/N)");
-      string userAnswer = Console.ReadLine();
-      string userAnswerLower = userAnswer.ToLower();
-      if (userAnswerLower == "y")
-      {
-        FindAnimal();
-      }
-      else
-      {
-        Console.WriteLine("Would you like to add an animal to the wildlife tracker instead, or update information for an existing animal in the tracker? (add/update)");
-        string answerInput = Console.ReadLine();
-        string answerInputLower = answerInput.ToLower();
-        if (answerInputLower == "add")
-        {
-          AddAnimal();
-        }
-        else if (answerInputLower == "update")
-        {
-          UpdateAnimalAge();
-        }
-        else
-        {
-          ReturnToMenu();
-        }
-      }
+      Console.ResetColor();
+      Console.BackgroundColor = ConsoleColor.Cyan;
+      Console.ForegroundColor = ConsoleColor.Black;
+      SelectOption();
     }
   }
 }
